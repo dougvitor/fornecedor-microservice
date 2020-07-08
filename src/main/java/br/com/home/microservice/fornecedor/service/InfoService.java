f935aebca92a2795bb585a0341e5a3a07be7d197
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2020-07-07 23:15:19 -0300

    Aula 02 -  Service Registry com Eureka
    O que foi feito:
    
       - A integração entre microsserviços com RestTemplate
            - O RestTemplate do Spring permite chamadas HTTP de alto nível
    
        - Um introdução ao Service discovery e Service registry
            - Service registry é um servidor central, onde todos os microsserviços ficam cadastrados (nome e IP/porta)
            - Service discovery é um mecanismo de descoberta do IP do microsserviço pelo nome
            - Dessa forma, nenhum microsserviço fica acoplado ao outro pelo IP/porta
        - A implementação do service registry através do Eureka Server
        - Como registrar da Loja e do Fornecedor no Eureka Server
        - A resolução do IP/porta através do nome do microsserviço nas requisições

---
+++ src/main/java/br/com/home/microservice/fornecedor/service/InfoService.java
@@ -0,0 +1,19 @@
+package br.com.home.microservice.fornecedor.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.home.microservice.fornecedor.model.InfoFornecedor;
+import br.com.home.microservice.fornecedor.repository.InfoRepository;
+
+@Service
+public class InfoService {
+	
+	@Autowired
+	private InfoRepository repository;
+
+	public InfoFornecedor getInfoPorEstado(String estado) {
+		return repository.findByEstado(estado);
+	}
+
+}
